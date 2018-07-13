4895414d8ee9338416315fdeec4ca7c9e654201a
clemente77 <40859087+clemente77@users.noreply.github.com>
2018-07-13 12:14:59 -0300

    introduccion switch
    logrado

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,31 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+	switch(mesDelAño)
+	{
+		case"Julio":
+		case "Agosto":
+			alert("abrigate que hace frio");
+			break;
+
+		case"Septiembre":
+		case"Octubre":
+		case"Noviembre":
+		case"Diciembre":
+			alert("ya pasamos el frio ahora calor");
+			break;
+
+
+		default:
+			alert("falta para el invierno");
+			break;
+
+
+	}
+
+
+
+
 
 
 
