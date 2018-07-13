353e51e2cb511996f761c85b2429635d12fd6fe9
clemente77 <40859087+clemente77@users.noreply.github.com>
2018-07-13 12:21:05 -0300

    swicth
    logrado

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -5,7 +5,23 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+	switch(mesDelAño)
+		{
+			case "Febrero":
+			alert("este mes no tiene mas de 30 dias");
+			break;
+
+			default:
+			alert("este mes tiene 30 o mas dias");
+			break;
+
+
+
+
+
+
+		}
+
 
 
 }//FIN DE LA FUNCIÓN
